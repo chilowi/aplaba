5789e57df75fd021a87ec7577ada733c653480fa
chilowi <chilowi@u-pem.fr>
2016-02-17 11:50:18 +0100

    w

--- exos/test/env.pl
+++ exos/test/env.pl
@@ -2,5 +2,14 @@ title=Environnement
 author=chilowi
 grader==
 import os, json
-print(json.dumps(dict(os.environ)))
+import student
+
+def get_useful_data():
+	data = {}
+	data.update(globals={ str(k): str(v) for (k, v) in globals().items() })
+	data.update(environ=dict(os.environ))
+	return data
+	
+print(json.dumps(get_useful_data()))
+
 ==
